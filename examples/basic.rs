c2b7b0f7a168a9356bcb8d2c5339763dc0f109af
Andreas Longva <andreas.b.longva@gmail.com>
2019-05-18 09:34:08 +0200

    Additional examples

--- examples/basic.rs
+++ examples/basic.rs
@@ -18,7 +18,27 @@ fn test2<T>() -> T
     3 / 2 / 2
 }
 
+#[replace_numeric_literals(T::try_from(literal).map_err(|_| ())?)]
+fn test3<T>() -> Result<T, ()>
+    where T: TryFrom<i8>+ Div<Output=T>
+{
+    let res = 3 / 2 / 2;
+    Ok(res)
+}
+
+#[replace_numeric_literals(f32::from(literal))]
+pub trait A {}
+
+#[allow(dead_code)]
+struct B;
+
+#[replace_numeric_literals(literal)]
+impl B {
+
+}
+
 fn main() {
     println!("{}", test::<f64>());
     println!("{}", test2::<f64>());
+    println!("{}", test3::<f64>().unwrap());
 }
