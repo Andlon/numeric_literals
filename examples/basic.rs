7a0325d246089a240bda1237584cc511d6f5dd49
Andreas Longva <andreas.b.longva@gmail.com>
2019-05-18 09:10:23 +0200

    Make replacement expression explicit

--- examples/basic.rs
+++ examples/basic.rs
@@ -2,12 +2,23 @@ extern crate numliterals;
 
 use numliterals::numeric_literals;
 use std::ops::Div;
+use std::convert::TryFrom;
+use std::fmt::Debug;
 
-#[numeric_literals(T)]
+#[numeric_literals(T::from(literal))]
 fn test<T>() -> T where T: From<i8> + Div<Output=T> {
+    3 / 2 / 2
+}
+
+#[numeric_literals(T::try_from(literal).expect("Must fit"))]
+fn test2<T>() -> T
+    where T: TryFrom<i8>+ Div<Output=T>,
+          <T as TryFrom<i8>>::Error: Debug
+{
     3 / 2 / 2
 }
 
 fn main() {
     println!("{}", test::<f64>());
+    println!("{}", test2::<f64>());
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,71 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
 
-use syn::{parse_macro_input, Expr, Item};
+use syn::{parse_macro_input, Expr, Item, ExprLit};
 use syn::visit_mut::{VisitMut, visit_item_mut, visit_expr_mut};
 
 use quote::quote;
 use quote::ToTokens;
+use syn::punctuated::Pair;
 
-struct NumericLiteralReplacer {
-    pub typename: String
+/// Visit an expression and replaces any numeric literal
+/// with the replacement expression, in which a placeholder identifier
+/// is replaced with the numeric literal.
+struct NumericLiteralVisitor {
+    pub placeholder: String,
+    pub replacement: Expr
 }
 
-impl VisitMut for NumericLiteralReplacer {
-    fn visit_expr_mut(&mut self, i: &mut Expr) {
-        match i  {
-            Expr::Lit(expr) => {
-                let replaced = format!("{}::from({})", &self.typename, expr.into_token_stream().to_string());
-                *i = syn::parse_str(&replaced).unwrap();
+impl VisitMut for NumericLiteralVisitor {
+    fn visit_expr_mut(&mut self, expr: &mut Expr) {
+        match expr  {
+            Expr::Lit(lit_expr) => {
+                let mut adapted_replacement = self.replacement.clone();
+                let mut replacer = ReplacementExpressionVisitor {
+                    placeholder: self.placeholder.clone(),
+                    literal: lit_expr.clone()
+                };
+
+                replacer.visit_expr_mut(&mut adapted_replacement);
+                *expr = adapted_replacement;
             },
-            _ => visit_expr_mut(self, i)
+            _ => visit_expr_mut(self, expr)
         }
+    }
+}
+
+/// Visits the "replacement expression", which replaces a placeholder identifier
+/// with the given literal.
+struct ReplacementExpressionVisitor {
+    pub placeholder: String,
+    pub literal: ExprLit
+}
+
+impl VisitMut for ReplacementExpressionVisitor {
+    fn visit_expr_mut(&mut self, expr: &mut Expr) {
+        match expr {
+            Expr::Path(path_expr) => {
+                if let Some(last_pair) = path_expr.path.segments.last() {
+                    if let Pair::End(segment) = last_pair {
+                        if segment.ident.to_string() == self.placeholder {
+                            *expr = Expr::Lit(self.literal.clone());
+                            return;
+                        }
+                    }
+                }
+            },
+            _ => {}
+        }
+        visit_expr_mut(self, expr)
     }
 }
 
 #[proc_macro_attribute]
 pub fn numeric_literals(attr: TokenStream, item: TokenStream) -> TokenStream {
-    let numeric_type = attr.to_string().trim().to_string();
-
     let mut input = parse_macro_input!(item as Item);
+    let attributes_tree = parse_macro_input!(attr as Expr);
 
-    let mut replacer = NumericLiteralReplacer { typename: numeric_type };
+    let mut replacer = NumericLiteralVisitor { placeholder: "literal".to_string(), replacement: attributes_tree };
 
     visit_item_mut(&mut replacer, &mut input);
 
