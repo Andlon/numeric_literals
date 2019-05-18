4bd43bff3801a4a75afae7a1d3c6997f567612ce
Andreas Longva <andreas.b.longva@gmail.com>
2019-05-18 10:30:11 +0200

    Tests for conversions

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,6 +1,7 @@
 extern crate numeric_literals;
 
 use numeric_literals::replace_numeric_literals;
+use std::ops::Add;
 
 #[test]
 fn leaves_string_untouched() {
@@ -50,4 +51,58 @@ fn leaves_raw_untouched() {
     }
 
     assert_eq!(gen_raw(), r#"test"#);
+}
+
+#[test]
+fn converts_integers_to_f32() {
+    #[replace_numeric_literals(literal as f32)]
+    fn gen_f32() -> f32 {
+        3
+    }
+
+    assert_eq!(gen_f32(), 3.0);
+}
+
+#[test]
+fn converts_floats_to_i32() {
+    #[replace_numeric_literals(literal as i32)]
+    fn gen_i32() -> i32 {
+        3.2
+    }
+
+    assert_eq!(gen_i32(), 3);
+}
+
+#[test]
+fn converts_generic_with_from() {
+    #[replace_numeric_literals(T::from(literal))]
+    fn gen<T: From<i8>>() -> T {
+        3
+    }
+
+    assert_eq!(gen::<f32>(), 3.0);
+    assert_eq!(gen::<f64>(), 3.0);
+    assert_eq!(gen::<i8>(), 3);
+    assert_eq!(gen::<i16>(), 3);
+    assert_eq!(gen::<i32>(), 3);
+    assert_eq!(gen::<i64>(), 3);
+    assert_eq!(gen::<i128>(), 3);
+}
+
+#[test]
+fn converts_generic_arithmetic_with_from() {
+    #[replace_numeric_literals(T::from(literal))]
+    fn gen<T>() -> T
+        where T: From<i8> + Add<T, Output=T>
+    {
+        3 + 2
+    }
+
+    assert_eq!(gen::<f32>(), 5.0);
+    assert_eq!(gen::<f64>(), 5.0);
+    assert_eq!(gen::<i8>(), 5);
+    assert_eq!(gen::<i16>(), 5);
+    assert_eq!(gen::<i32>(), 5);
+    assert_eq!(gen::<i64>(), 5);
+    assert_eq!(gen::<i128>(), 5);
 }
