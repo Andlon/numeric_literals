9a13bb7483af69776c03b62b49e86b16825ad82d
Andreas Longva <andreas.b.longva@gmail.com>
2019-05-19 15:56:27 +0200

    Improve documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 //!
-//! **numeric_literals** is a Rust library that provides  procedural attribute-macro for replacing numeric literals
-//! with arbitrary expressions.
+//! **numeric_literals** is a Rust library that provides procedural attribute macros for replacing
+//! numeric literals with arbitrary expressions.
 //!
 //! While Rust's explicitness is generally a boon, it is a major pain when writing numeric
-//! code that is intended to be generic over its scalar type. As an example, consider
+//! code that is intended to be generic over a scalar type. As an example, consider
 //! writing a function that returns the golden ratio for any type that implements `T: num::Float`.
 //! An implementation might look like the following.
 //!
@@ -39,10 +39,48 @@
 //! we move the conversion behavior away from where the literals are needed, enhancing readability
 //! by reducing the noise imposed by being explicit about the exact types involved.
 //!
-//! That said, the macro should be used with care. It is recommended to keep the macro close to
+//! Float and integer literal replacement
+//! -------------------------------------
+//!
+//! An issue with the replacement of numeric literals is that there is no way to dinstinguish
+//! literals that are used for e.g. indexing from those that are part of a numerical computation.
+//! In the example above, if you would additionally need to index into an array with a constant index
+//! such as `array[0]`, the macro will try to convert the index `0` to a float type, which
+//! would clearly fail. Thankfully, in most cases these examples will outright fail to compile
+//! because of type mismatch. One possible resolution to this problem is to use the separate
+//! macros `replace_float_literals` and `replace_int_literals`, which work in the exact same way,
+//! but only trigger on float or integer literals, respectively. Below is an example from
+//! Finite Element code that uses float literal replacement to improve readability of numerical
+//! constants in generic code.
+//!
+//! ```ignore
+//! #[replace_float_literals(T::from_f64(literal).expect("Literal must fit in T"))]
+//! pub fn assemble_element_mass<T>(quad: &Quad2d<T>) -> MatrixN<T, U8>
+//! where
+//!    T: RealField
+//! {
+//!     let phi = |alpha, beta, xi: &Vector2<T>| -(1.0 + alpha * xi[0]) * (1.0 + beta * xi[1]) / 4.0;
+//!     let phi_grad = |alpha, beta, xi: &Vector2<T>| {
+//!         Vector2::new(
+//!             alpha * (1.0 + beta * xi[1]) / 4.0,
+//!             beta * (1.0 + alpha * xi[0]) / 4.0,
+//!         )
+//!     };
+//!     let alphas = [-1.0, 1.0, 1.0, -1.0];
+//!     let betas = [-1.0, -1.0, 1.0, 1.0];
+//!
+//!     // And so on...
+//! ```
+//!
+//! In general, **the macros should be used with caution**. It is recommended to keep the macro close to
 //! the region in which the literals are being used, as to avoid confusion for readers of the code.
-//! The Rust code that is usually not valid Rust (because of the lack of explicit conversion),
-//! but without the context of the attribute, it is simply not clear why this code still compiles.
+//! The Rust code before macro expansion is usually not valid Rust (because of the lack of explicit
+//! type conversion), but without the context of the attribute, it is simply not clear why this
+//! code still compiles.
+//!
+//! An option for the future would be to apply the attribute only to very local blocks of code that
+//! are heavy on numerical constants. However, at present, Rust does not allow attribute macros
+//! to apply to blocks or single expressions.
 
 extern crate proc_macro;
 use proc_macro::TokenStream;
